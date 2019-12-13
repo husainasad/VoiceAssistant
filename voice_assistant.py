db3afc8ef11eb73a5a3baf8b1d6db7c5e74ff541
husainasad <shusain_be16@thapar.edu>
2019-12-13 05:56:04 +0530

    setting up Google Calendar API and getting date from speech

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,9 +1,21 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 import os
 import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
 
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+MONTHS = ["january", "february", "march", "april", "may", "june","july", "august", "september","october", "november", "december"]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 def speak(text):
 	tts = gTTS(text=text, lang ="en")
 	filename = "voice.mp3"
@@ -25,10 +37,104 @@ def get_audio():
 	return said
 
 # speak("hello there")
-text = get_audio()
+# text = get_audio()
 
-if "hello" in text:
-	speak("hello! how are you?")
+# if "hello" in text:
+# 	speak("hello! how are you?")
 
-if "what is your name" in text:
-	speak("My name is cortana")
+# if "what is your name" in text:
+# 	speak("My name is cortana")
+
+def authenticate_google():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
+def get_events(n, service):
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print(f'Getting the upcoming {n} events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=n, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+def get_date(text):
+	text = text.lower()
+	today = datetime.date.today()
+
+	if text.count("today") > 0:
+		return today
+
+	day = -1
+	day_of_week = -1
+	month = -1
+	year = today.year
+
+	for word in text.split():
+		if word in MONTHS:
+			month = MONTHS.index(word) + 1
+		elif word in DAYS:
+			day_of_week = DAYS.index(word)
+		elif word.isdigit():
+			day = int(word)
+		else:
+			for ext in DAY_EXTENTIONS:
+				found = word.find(ext)
+				if found > 0:
+					try:
+						day = int(word[:found])
+					except:
+						pass
+
+	if month < today.month and month != -1:
+		year += 1
+
+	if day < today.day and month != -1 and day != -1:
+		month += 1
+
+	if month == -1 and day == -1 and day_of_week != -1:
+		current_day_of_week = today.weekday()
+		dif =day_of_week - current_day_of_week
+
+		if dif < 0:
+			dif += 7
+			if text.count("next") >= 1:
+				dif += 7
+
+		return today + datetime.timedelta(dif)
+
+	return datetime.date(month = month, day = day, year = year)
+
+# service = authenticate_google()
+# get_events(2, service)
+
+text = get_audio().lower()
+print(get_date(text))
